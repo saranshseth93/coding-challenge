3b30e70066551639fab0639a872a3a9a7331cfe1
Saransh <saranshs@nookal.com>
2021-07-07 13:51:39 +0530

    Bootstrap fixes

--- dist/coding-challenge.e31bb0bc.js
+++ dist/coding-challenge.e31bb0bc.js
@@ -11251,7 +11251,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61920" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59149" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

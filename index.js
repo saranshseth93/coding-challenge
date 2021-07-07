41b6f783811029624f39d0d80c03507d49d9e205
Saransh <saranshs@nookal.com>
2021-07-07 13:01:57 +0530

    Initial design

---
+++ index.js
@@ -0,0 +1,9 @@
+var $ = require("jquery");
+$(document).ready(function (e) {
+  console.log("hi");
+  setTimeout(function () {
+    $(".loader").addClass("hidden");
+    $(".content").fadeIn("slow");
+    console.log("hello");
+  }, 3000);
+});
